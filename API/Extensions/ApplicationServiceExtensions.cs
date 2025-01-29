9cb8c6c06c6515c604a11a93b11cc02b7c2f9b40
Riley <rileywalshh@gmail.com>
2025-01-29 10:53:27 +1000

    Section 8 done

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using API.Data;
+using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -15,6 +16,8 @@ public static class ApplicationServiceExtensions
         });
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
     }
