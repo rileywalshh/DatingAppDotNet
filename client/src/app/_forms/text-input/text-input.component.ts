c2c74befbf4a5e64fc6579ea585530419412650d
Riley <rileywalshh@gmail.com>
2025-02-11 16:20:27 +1000

    input comps added

---
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,28 @@
+import { Component, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.css'
+})
+export class TextInputComponent implements ControlValueAccessor{
+  label = input<string>('');
+  type = input<string>('text');
+
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this
+  }
+  writeValue(obj: any): void {
+  }
+  registerOnChange(fn: any): void {
+  }
+  registerOnTouched(fn: any): void {
+  }
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl
+  }
+}
